049b2140e1ede479bfae6cda8468f2a6e2a4e9e7
P. Todd Decker <ptdecker@mac.com>
2024-12-02 21:31:28 -0600

    Day 02 Part 1 puzzle solution

--- day02part1/src/main.rs
+++ day02part1/src/main.rs
@@ -1,7 +1,57 @@
 //! Advent of Code Day Two Part One
 
-use lib::Result;
+use lib::{read_rows, Result};
+
+use crate::Direction::NotEstablished;
+
+/// The direction the report vector is going
+#[derive(Default)]
+enum Direction {
+    #[default]
+    NotEstablished,
+    Down,
+    Up,
+}
+
+/// Check to see if the "report" (represented by the row data vector) is safe
+fn safety_check(row_data: Vec<u32>) -> bool {
+    let mut direction: Direction = NotEstablished;
+    let mut prior_sentinel: Option<u32> = None;
+    for value in row_data {
+        if let Some(prior) = prior_sentinel {
+            // Make sure were going in the same direction. Or, if we haven't established direction,
+            // then establish it.
+            match direction {
+                // If we haven't established a direction yet, establish it
+                NotEstablished if value > prior => direction = Direction::Up,
+                NotEstablished if value < prior => direction = Direction::Down,
+                // If we're going in the same direction as before, then pass
+                Direction::Down if value < prior => (),
+                Direction::Up if value > prior => (),
+                // In any other case, we aren't safe and can short-circuit fail
+                _ => return false,
+            }
+            // We checked above to see if we were moving at least once. But, we still need to make
+            // sure we do not jump by more than three.
+            if value.abs_diff(prior) > 3 {
+                return false;
+            }
+        }
+        prior_sentinel = Some(value);
+    }
+    true
+}
 
 fn main() -> Result<()> {
+    println!(
+        "{}",
+        read_rows()?
+            .into_iter()
+            // convert the row of the report into a true or false value
+            .map(safety_check)
+            // `safety_result as u32` gives u32 value of 0 to false and 1 to true
+            .map(|safety_result| safety_result as u32)
+            .sum::<u32>()
+    );
     Ok(())
 }
--- justfile
+++ justfile
@@ -20,16 +20,16 @@ day01part2: lint
 
 # Day 02, Part 1: Example Input Solution
 day02part1example: lint
-    cargo run --bin day01part1 < data/example01.txt
+    cargo run --bin day02part1 < data/example02.txt
 
 # Day 02, Part 1: Puzzle Input Solution
 day02part1: lint
-    cargo run --bin day01part1 < data/day01.txt
+    cargo run --bin day02part1 < data/day02.txt
 
 # Day 02, Part 2: Example Input Solution
 day02part2example: lint
-    cargo run --bin day01part2 < data/example01.txt
+    cargo run --bin day02part2 < data/example02.txt
 
 # Day 02, Part 2: Puzzle Input Solution
 day02part2: lint
-    cargo run --bin day01part2 < data/day01.txt
+    cargo run --bin day02part2 < data/day02.txt
